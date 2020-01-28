aae2f4f18e2fd7d9b8da6954e3f89bcf6a530b90
chrisYoder <christophermyoder@gmail.com>
2020-01-28 21:14:28 +0000

    01/28 eod. Need to finish admin routing

---
+++ angular-portfolio/src/app/admin/admin-routing/admin-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProjectComponent } from '../project/project.component';
+import { ProjectListComponent } from '../project-list/project-list.component';
+import { ProjectCreateComponent } from '../project-create/project-create.component';
+import { ProjectUpdateComponent } from '../project-update/project-update.component';
+
+const routes: Routes = [
+	{ path: 'admin', component: ProjectComponent }, 
+	{ path: 'list', component: ProjectListComponent }, 
+	{ path: 'create', component: ProjectCreateComponent }, 
+	{ path: 'update', component: ProjectUpdateComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ], 
+  exports: [
+	  RouterModule
+  ]
+})
+export class AdminRoutingModule { }
---
+++ angular-portfolio/src/app/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing/admin-routing.module';
+
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectCreateComponent } from './project-create/project-create.component';
+import { ProjectUpdateComponent } from './project-update/project-update.component';
+import { ProjectComponent } from './project/project.component';
+
+
+
+@NgModule({
+  declarations: [ProjectListComponent, ProjectCreateComponent, ProjectUpdateComponent, ProjectComponent],
+  imports: [
+    CommonModule, 
+	AdminRoutingModule
+  ]
+})
+export class AdminModule { }
